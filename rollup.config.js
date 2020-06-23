29e3c21701946a140971caa2cbf16e0937c66dc6
Naftali Lubin <naftalilubin@gmail.com>
2020-06-23 12:20:50 -0400

    feat: 🎸 Update rollup and babel build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import filesize from 'rollup-plugin-filesize';
 import license from 'rollup-plugin-license';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 const pkg = require('./package.json');
 
@@ -9,27 +10,35 @@ ${pkg.homepage}
 @author ${pkg.author}
 @preserve`;
 
+
+const plugins = [
+  babel({
+    babelHelpers: 'runtime',
+    exclude: 'node_modules/**'
+  }),
+  filesize(),
+  license({
+    banner,
+  }),
+];
+
 const config = {
-  external: ['react', 'prop-types'],
+  external: ['react', 'prop-types', /@babel\/runtime\/.*/],
   input: 'src/index.js',
-  output: {
-    exports: 'named',
+  output: [{
     file: 'dist/index.js',
-    format: 'umd',
-    name: 'ReactBadly',
-    sourcemap: true,
-    globals: {
-      'react': 'React',
-      'prop-types': 'PropTypes'
-    }
-  },
-  plugins: [
-    babel(),
-    filesize(),
-    license({
-      banner,
-    }),
-  ]
+    format: 'cjs'
+  },{
+    file: 'dist/index.min.js',
+    format: 'cjs',
+    plugins: [
+      terser()
+    ]
+  }, {
+    file: 'dist/index.esm.js',
+    format: 'es'
+  }],
+  plugins
 };
 
 export default config;
---
+++ src/ReactBadly.js
@@ -0,0 +1,36 @@
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+const dummyFunc = () => null;
+
+export class ReactBadly extends PureComponent {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch (error, info) {
+    const errorHandler = this.props.onError || dummyFunc;
+
+    this.setState({ hasError: true, errorInformation: { error, info } }, () => {
+      errorHandler(error, info);
+    });
+  }
+
+  render () {
+    if (this.state.hasError) {
+      if (this.props.render) {
+        return this.props.render(this.state.errorInformation);
+      }
+
+      return null;
+    }
+
+    return this.props.children;
+  }
+}
+
+ReactBadly.propTypes = {
+  children: PropTypes.node.isRequired,
+  onError: PropTypes.func,
+  render: PropTypes.func,
+};
